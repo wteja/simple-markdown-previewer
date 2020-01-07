ac2b5f4fbf070f9ccd68c73a46c354b1035a37b1
Weerayut Teja <teja.weerayut@gmail.com>
2020-01-07 08:21:20 +0700

    Adding sample data

--- src/main.js
+++ src/main.js
@@ -11,8 +11,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const preview = document.getElementById("preview");
 
     let defaultValue = "# Markdown Previewer\n\n";
+    defaultValue += "![Markdown Previewer](./image.png)\n\n";
     defaultValue += "## This is a simple markdown previewer made with HTML + CSS + Vanilla JavaScript\n\n";
-    defaultValue += "Check [Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) on how to use Markdown.";
+    defaultValue += "Check [Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) on how to use Markdown.\n\n";
+    defaultValue += "### Sample Code Block\n\n";
+    defaultValue += "```javascript\nvar message = \"Hello World\";\nconsole.log(message);\n```\n\n"
+    defaultValue += "This is `inline code`\n\n";
+    defaultValue += "And this is **bold text** and *italic text*\n\n";
+    defaultValue += "- Sample List Item 1\n- Sample List Item 2\n- Sample List Item 3\n\n";
+    defaultValue += "> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas gravida at nisl ut lobortis. Donec mattis risus a neque suscipit bibendum. Curabitur pellentesque, enim vitae sollicitudin iaculis, risus dui iaculis lorem, ac pulvinar neque odio nec est. Nulla volutpat gravida ligula eget fringilla. Quisque vestibulum, magna a rutrum fermentum, felis dui sodales elit, eget mattis urna eros vel magna. Proin in bibendum neque. Pellentesque aliquet diam nisi. Donec a tincidunt magna.\n\n";
     editor.value = defaultValue;
     previewMarkdown(editor.value, preview);
 
